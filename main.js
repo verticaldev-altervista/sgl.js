7ab0355d4884b1e9515ce1e4bac62a42fa122f26
vroby65 <vroby.mail@gmail.com>
2024-06-17 20:54:50 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 setdisplay();
 
+
 function update(){
 		cls();
-		text(display, 50,50,16,rgb(255,0,0),"hello world....");
+		text(display, 50,50,16,rgb(255,0,0),"hello world.... with sgl.js");
 	
 	
 }
